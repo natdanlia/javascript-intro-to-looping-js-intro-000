230ca508245d1e95b96979793683b5ed1df8ae75
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-09-20 20:02:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,9 +21,11 @@ function whileLoop(n) {
 }
 
 function doWhileLoop(array) {
-
-
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+}
 do {
   array.pop();
   } while (array.length > 0 && maybeTrue()) 
-}
+
